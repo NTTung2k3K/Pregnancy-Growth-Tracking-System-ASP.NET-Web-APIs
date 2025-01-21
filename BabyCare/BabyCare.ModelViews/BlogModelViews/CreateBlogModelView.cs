010412440956feafa96c2c7e0b5a6cf0fbdd0aad
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2025-01-21 20:22:35 +0700

    Completed API Blog and BlogType

---
+++ BabyCare/BabyCare.ModelViews/BlogModelViews/CreateBlogModelView.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyCare.ModelViews.BlogModelViews
+{
+    public class CreateBlogModelView
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(200, ErrorMessage = "Title cannot exceed 200 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "AuthorId is required.")]
+        public Guid AuthorId { get; set; }
+
+        public int? LikesCount { get; set; } = 0;
+
+        public int? ViewCount { get; set; } = 0;
+
+        public string? Status { get; set; }
+
+        public string? Sources { get; set; }
+
+        public IFormFile? Thumbnail { get; set; }
+
+        [Required(ErrorMessage = "BlogTypeId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "BlogTypeId must be a positive integer.")]
+        public int BlogTypeId { get; set; }
+
+        public bool IsFeatured { get; set; }
+    }
+}
---
+++ BabyCare/BabyCare.ModelViews/ChildModelView/UpdateChildModelView.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyCare.ModelViews.ChildModelView
+{
+    public class UpdateChildModelView
+    {
+        public Guid? UserId { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? FetalGender { get; set; }
+        public string? PregnancyStage { get; set; }
+        public float? WeightEstimate { get; set; }
+        public float? HeightEstimate { get; set; }
+        public string? DeliveryPlan { get; set; }
+        public string? Complications { get; set; }
+        public IFormFile? PhotoUrl { get; set; }
+        public string? BloodType { get; set; }
+        public string? PregnancyWeekAtBirth { get; set; }
+    }
+}
