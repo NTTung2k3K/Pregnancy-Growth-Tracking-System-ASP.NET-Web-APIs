3c8b40240b882d762e7772b7ce09bb7bcd58cfcf
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2025-02-12 16:16:46 +0700

    Add new properties to CreateMPRequest and UpdateMPRequest
    Updated CreateMPRequest and UpdateMPRequest classes in the BabyCare.ModelViews.MembershipPackageModelViews.Request namespace to include five new properties: MaxRecordAdded, MaxGrowthChartShares, HasGenerateAppointments, HasStandardDeviationAlerts, and HasViewGrowthChart. These properties enhance the functionality of the membership package requests by allowing for more detailed configurations.

--- BabyCare/BabyCare.ModelViews/MembershipPackageModelViews/Request/UpdateMPRequest.cs
+++ BabyCare/BabyCare.ModelViews/MembershipPackageModelViews/Request/UpdateMPRequest.cs
@@ -21,5 +21,12 @@ namespace BabyCare.ModelViews.MembershipPackageModelViews.Request
         public IFormFile? ImageUrl { get; set; }
         public decimal? Discount { get; set; }
         public int? ShowPriority { get; set; }
+
+        public int MaxRecordAdded { get; set; } // -1 = Unlimited
+
+        public int MaxGrowthChartShares { get; set; } // -1 = Unlimited
+        public bool HasGenerateAppointments { get; set; }   // 0 nếu không hỗ trợ
+        public bool HasStandardDeviationAlerts { get; set; }
+        public bool HasViewGrowthChart { get; set; }
     }
 }
