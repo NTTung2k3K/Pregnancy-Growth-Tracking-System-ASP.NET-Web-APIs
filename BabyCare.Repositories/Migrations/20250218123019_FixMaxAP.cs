4d1949bdcaef45fc1617ff3d26a9697985a0a6af
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2025-03-25 23:06:31 +0700

    deploy

---
+++ BabyCare.Repositories/Migrations/20250218123019_FixMaxAP.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BabyCare.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMaxAP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FetalGrowthRecords_GrowthCharts_GrowthChartId",
+                table: "FetalGrowthRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FetalGrowthRecords_GrowthChartId",
+                table: "FetalGrowthRecords");
+
+            migrationBuilder.DropColumn(
+                name: "GrowthChartId",
+                table: "FetalGrowthRecords");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentBookingCount",
+                table: "UserMemberships",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxAppointmentCanBooking",
+                table: "MembershipPackages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentBookingCount",
+                table: "UserMemberships");
+
+            migrationBuilder.DropColumn(
+                name: "MaxAppointmentCanBooking",
+                table: "MembershipPackages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GrowthChartId",
+                table: "FetalGrowthRecords",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FetalGrowthRecords_GrowthChartId",
+                table: "FetalGrowthRecords",
+                column: "GrowthChartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FetalGrowthRecords_GrowthCharts_GrowthChartId",
+                table: "FetalGrowthRecords",
+                column: "GrowthChartId",
+                principalTable: "GrowthCharts",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ BabyCare.Repositories/Migrations/20250223163040_jlfajsd.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BabyCare.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class jlfajsd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "MembershipPackages");
+
+            migrationBuilder.DropColumn(
+                name: "HeightEstimate",
+                table: "Childs");
+
+            migrationBuilder.DropColumn(
+                name: "PregnancyWeekAtBirth",
+                table: "Childs");
+
+            migrationBuilder.DropColumn(
+                name: "WeightEstimate",
+                table: "Childs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "MembershipPackages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "HeightEstimate",
+                table: "Childs",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PregnancyWeekAtBirth",
+                table: "Childs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "WeightEstimate",
+                table: "Childs",
+                type: "real",
+                nullable: true);
+        }
+    }
+}
