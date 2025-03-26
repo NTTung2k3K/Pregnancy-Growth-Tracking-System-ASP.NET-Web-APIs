75692de6a634dc46a2936b341a739e1e5c26cb11
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2025-03-26 19:00:40 +0700

    fix db

--- BabyCare.CronJobs/Program.cs
+++ BabyCare.CronJobs/Program.cs
@@ -1,8 +1,20 @@
 using BabyCare.CronJobs;
 using BabyCare.CronJobs.Worker;
+using BabyCare.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 var builder = Host.CreateApplicationBuilder(args);
-
+// config appsettings by env
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseLazyLoadingProxies().UseMySQL(builder.Configuration.GetConnectionString("BabyCareDb"));
+});
 builder.Services.AddHostedService<ReminderWorker>();
 builder.Services.AddHostedService<FetalGrowthAlertWorker>();
 builder.Services.AddHostedService<AppointmentWorker>();
