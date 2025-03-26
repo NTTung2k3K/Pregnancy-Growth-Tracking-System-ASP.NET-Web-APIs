f6507144506493a3967e71723acc0f69bd22aab9
Thang Lam <thanglam271003@gmail.com>
2025-03-26 22:17:14 +0700

    chat real-time

---
+++ BabyCare.Contract.Services/Interface/IRealTimeService.cs
@@ -0,0 +1,13 @@
+﻿using BabyCare.Contract.Repositories.Entity;
+using BabyCare.Core.APIResponse;
+using System.Threading.Tasks;
+
+namespace BabyCare.Contract.Services.Interface
+{
+    public interface IRealTimeService
+    {
+        Task SendMessage(string channel, string message);
+        Task<ApiResult<string>> CheckUserRole(string userName);
+    }
+}
+
