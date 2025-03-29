b522dc57b729e125587c799e40a01abb13626248
LilHuwng <nhat040104@gmail.com>
2025-03-29 07:29:19 +0700

    Fix Membership

--- BabyCare.API/Controllers/MembershipPackagesController.cs
+++ BabyCare.API/Controllers/MembershipPackagesController.cs
@@ -4,7 +4,6 @@ using BabyCare.Core;
 using BabyCare.ModelViews.MembershipPackageModelViews.Request;
 using BabyCare.ModelViews.UserModelViews.Response;
 using Microsoft.AspNetCore.Mvc;
-using StackExchange.Redis;
 
 namespace BabyCare.API.Controllers
 {
@@ -14,11 +13,11 @@ namespace BabyCare.API.Controllers
     {
         private readonly IMembershipPackageService _membershipPackageService;
 
+
         public MembershipPackagesController(IMembershipPackageService membershipPackageService)
         {
             _membershipPackageService = membershipPackageService;
         }
-
 
         [HttpPost("create")]
         public async Task<IActionResult> CreateMembershipPackage([FromForm] CreateMPRequest request)
@@ -33,7 +32,64 @@ namespace BabyCare.API.Controllers
                 return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
             }
         }
+        [HttpGet("callbackvnpay")]
+        public async Task<IActionResult> HandleIpnActionVNpayBackEnd()
+        {
+            try
+            {
+                //var resultDescription = $"{paymentResult.PaymentResponse.Description}. {paymentResult.TransactionStatus.Description}.";
 
+
+                var result = await _membershipPackageService.HandleIpnActionVNpayBackEnd(Request.Query);
+                //return Ok(result);
+                return Redirect(result.ResultObj);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                //var resultDescription = $"{paymentResult.PaymentResponse.Description}. {paymentResult.TransactionStatus.Description}.";
+
+
+                var result = await _membershipPackageService.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
+        public static string GetIpAddress(HttpContext context)
+        {
+            var ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+            if (string.IsNullOrEmpty(ip))
+            {
+                ip = context.Connection.RemoteIpAddress?.ToString();
+            }
+            return ip ?? "0.0.0.0"; // Tránh null
+        }
+
+        [HttpPost("buy-package")]
+        public async Task<IActionResult> BuyPackage([FromBody] BuyPackageRequest request)
+        {
+            try
+            {
+                var ipAddress = MembershipPackagesController.GetIpAddress(HttpContext);
+                var result = await _membershipPackageService.BuyPackage(request, ipAddress);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
         [HttpPut("update")]
         public async Task<IActionResult> UpdateMembershipPackage([FromForm] UpdateMPRequest request)
         {
@@ -47,7 +103,6 @@ namespace BabyCare.API.Controllers
                 return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
             }
         }
-
         [HttpDelete("delete")]
         public async Task<IActionResult> DeleteMembershipPackage([FromQuery] DeleteMPRequest request)
         {
@@ -61,21 +116,6 @@ namespace BabyCare.API.Controllers
                 return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
             }
         }
-
-        [HttpGet("get-all")]
-        public async Task<IActionResult> GetAll()
-        {
-            try
-            {
-                var result = await _membershipPackageService.GetAll();
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
-            }
-        }
-
         [HttpGet("get-pagination")]
         public async Task<IActionResult> GetMembershipPackagePagination([FromQuery] BaseSearchRequest request)
         {
@@ -89,14 +129,87 @@ namespace BabyCare.API.Controllers
                 return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<BasePaginatedList<EmployeeResponseModel>>(ex.Message));
             }
         }
-
+        [HttpGet("get-membership-package-status-handler")]
+        public IActionResult GetMembershipPackageStatusHandler()
+        {
+            try
+            {
+                var result = _membershipPackageService.GetMembershipPackageStatusHandler();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<BasePaginatedList<EmployeeResponseModel>>(ex.Message));
+            }
+        }
         [HttpGet("get-by-id")]
         public async Task<IActionResult> GetMembershipPackageById([FromQuery] int id)
         {
             try
             {
                 var result = await _membershipPackageService.GetMembershipPackageById(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<BasePaginatedList<EmployeeResponseModel>>(ex.Message));
+            }
+        }
 
+
+
+        [HttpGet("can-added-record")]
+        public async Task<IActionResult> CanAddedRecord([FromQuery] Guid id)
+        {
+            try
+            {
+                var result = await _membershipPackageService.CanAddedRecord(id);
+                return Ok(new BabyCare.Core.APIResponse.ApiSuccessResult<bool>(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
+
+
+
+        [HttpGet("can-generate-appointment")]
+        public async Task<IActionResult> CanGenerateAppointments([FromQuery] Guid id)
+        {
+            try
+            {
+                var result = await _membershipPackageService.CanGenerateAppointments(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
+
+
+        [HttpGet("can-share-growth-chart")]
+        public async Task<IActionResult> CanShareGrowthChart([FromQuery] Guid id)
+        {
+            try
+            {
+                var result = await _membershipPackageService.CanShareGrowthChart(id);
+                return Ok(new BabyCare.Core.APIResponse.ApiSuccessResult<bool>(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BabyCare.Core.APIResponse.ApiErrorResult<object>(ex.Message));
+            }
+        }
+
+
+        [HttpGet("can-view-growth-chart")]
+        public async Task<IActionResult> CanViewGrowthChart([FromQuery] Guid id)
+        {
+            try
+            {
+                var result = await _membershipPackageService.CanViewGrowthChart(id);
                 return Ok(result);
             }
             catch (Exception ex)
@@ -106,3 +219,4 @@ namespace BabyCare.API.Controllers
         }
     }
 }
+
